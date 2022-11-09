2e9b879d64096aa7719ad6acdae4d07153b5d990
Amey Joshi <ameyjoshi2001rj@gmail.com>
2022-11-09 22:48:42 +0530

    Solved MCQ problem

---
+++ src/interview_problems/problem1.java
@@ -0,0 +1,12 @@
+package interview_problems;
+
+public class problem1 {
+    public static void main(String[] args) {
+//        int a=1,b=5,sum=0,i;
+//        for (;i<b;i++) {
+//            sum+=a;
+//            System.out.println(sum-b);
+//        }
+//Ans : error at line  for (;i<b;i++) {
+    }
+}

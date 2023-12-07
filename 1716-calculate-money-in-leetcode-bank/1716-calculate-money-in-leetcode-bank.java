f6364f5c1b03651f321f70cadfe6ce1f27438d1d
Amey Joshi <ameyjoshi2001rj@gmail.com>
2023-12-07 10:32:17 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (43.74%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
@@ -1,14 +1,40 @@
 class Solution {
+    // Method 1
+    // public int totalMoney(int n) {
+    //     int ans = 0;
+    //     int monday = 1;  
+    //     while(n>0){
+    //         for(int i = 0;i< Math.min(n,7);i++){
+    //         ans+=monday+i;
+    //     }
+    //         n-=7;
+    //         monday++;
+    //     }
+    //     return ans;
+    // }
     public int totalMoney(int n) {
-        int ans = 0;
-        int monday = 1;  
-        while(n>0){
-            for(int i = 0;i< Math.min(n,7);i++){
-            ans+=monday+i;
+       
+       // Initialization
+        int div = n/7;
+        int rem = n%7;
+        int start = 1;
+        int sum = 0;
+        
+        // Gauss Theorem formula to calculate sum and start value
+        // iteration happens until value div is reached
+        for(int i = 0;i<div;i++){
+            int end  = start +6;
+            int temp = ((start+end)*7)/2;
+            sum+=temp;
+            
+            start++;
         }
-            n-=7;
-            monday++;
+        
+        // To calculate the remaing days
+        // iteration happens until value rem is reached
+        for(int i = 0;i<rem;i++){
+            sum+=start++;
         }
-        return ans;
+        return sum;
     }
 }

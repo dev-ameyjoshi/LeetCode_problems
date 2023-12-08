17602d76ec281299bb8909275e58d714a169d7ea
Amey Joshi <ameyjoshi2001rj@gmail.com>
2023-12-08 20:48:50 +0530

    Time: 21 ms (67.89%), Space: 48.6 MB (18.18%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -28,7 +28,7 @@ class Solution {
         
        int[][] t = new int[m+1][n+1];
         
-        // if(n == 0 || m == 0) return 0;
+        if(n == 0 || m == 0) return 0;
         
         for(int i = 1;i<m+1;i++){
             for(int j = 1;j<n+1;j++){

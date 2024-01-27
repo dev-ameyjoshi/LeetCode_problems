4bad0588786679ad0173b46229339209e5c0b9e7
Amey Joshi <ameyjoshi2001rj@gmail.com>
2024-01-27 23:25:10 +0530

    Time: 62 ms (17.79%), Space: 42 MB (5.66%) - LeetHub

--- 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.java
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.java
@@ -1,6 +1,5 @@
 class Solution {
-    int left = 0;
-    int right = 0;
+   
     
     int t[][] = new int[3][1001];
     public int twoEggDrop(int n) {
@@ -28,26 +27,13 @@ class Solution {
         for(int k = 1;k<=f;k++){
         int high;
         int low;
-            // int temp = 1 + Math.max(solve(n-1,e-1),solve(n-k,e));
             
-//             if(t[e-1][f-1] != -1){
-//                 low = t[f-1][e-1];
-//             }else{
-//                 low = solve(f-1,e-1);
-//                 t[f-1][e-1] = low;
-//             }
-            
-//             if(t[f-k][e] != -1){
-//                 high = t[f-k][e];
-//             }else{
-//                 high = solve(f-k,e);
-//                 t[f-k][e] = high;
-//             }
             int temp=1+Math.max(solve(e-1,k-1),solve(e,f-k));
-            // int temp = 1 + low + high;
+
             mn = Math.min(mn,temp);
         }
         t[e][f] = mn;
         return mn;
     }
+
 }

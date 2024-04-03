d5a69290a52a695a5b99acd11e9475905e2676bb
Amey Joshi <ameyjoshi2001rj@gmail.com>
2024-04-03 23:20:57 +0530

    Time: 140 ms (66.67%), Space: 41.8 MB (29.73%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,42 @@
+class Solution {
+    
+    boolean visited[][];
+    public boolean exist(char[][] board, String word) {
+        
+        int m = board.length;
+        int n = board[0].length;
+        
+        visited = new boolean[m][n];
+        
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+             if(word.charAt(0) == board[i][j] && searchWord(i , j , 0 , board , word)){
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+               
+    }
+    
+    public boolean searchWord(int i , int  j , int index , char board[][], String word){
+ 
+        if(index == word.length()) return true;
+      
+        if(i < 0 || i >= board.length || j < 0 || j>= board[i].length || word.charAt(index) != board[i][j] || visited[i][j]){
+            return false;
+        }
+        
+        visited[i][j] = true;
+        if(searchWord(i+1, j, index+1,board,word) || 
+           searchWord(i-1, j, index+1,board,word) ||
+           searchWord(i   ,j+1, index+1,board,word) ||
+           searchWord(i   ,j-1, index+1,board,word) ){
+            return true;
+        }
+        
+        visited[i][j] = false;
+         return false;
+    }
+}

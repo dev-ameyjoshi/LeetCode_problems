a2effcc33ef993646f8d99097651bbc1db459690
Amey Joshi <ameyjoshi2001rj@gmail.com>
2024-05-27 11:28:55 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (68.83%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,34 +1,41 @@
 class Solution {
-  private int solve(int [] nums,int val){
-      int start = 0;
-      int end = nums.length-1;
+//   private int solve(int [] nums,int val){
+//       int start = 0;
+//       int end = nums.length-1;
       
-      int idx = nums.length;
-      while(start <= end){
-        int mid = start + (end-start)/2;
+//       int idx = nums.length;
+//       while(start <= end){
+//         int mid = start + (end-start)/2;
         
-        if(nums[mid] >= val){
-          idx = mid;
-          end = mid -1;
-        }else{
-          start = mid + 1;
-        }
-      }
-      
-      return idx;
-  }
+//         if(nums[mid] >= val){
+//           idx = mid;
+//           end = mid -1;
+//         }else{
+//           start = mid + 1;
+//         }
+//       }
+      // binary search method
+//       return idx;
+//   }
     public int specialArray(int[] nums) {
         //nums is special 
-      Arrays.sort(nums);
+      // Arrays.sort(nums);
+      int n = nums.length;
+      int [] freq = new int[n + 1];
       
-      for(int i = 1;i<=nums.length;i++){
+      for(int i = 0;i<n;i++){
         //if x = 0 then 
-        int k = solve(nums,i);
-        if(nums.length-k == i){
+        freq[Math.min(n,nums[i])]++;
+      }
+      
+      int num = 0;
+      
+      for(int i = n;i>=1;i--){
+        num += freq[i];
+        if(num == i){
           return i;
         }
       }
-      
       return -1;
     }
 }

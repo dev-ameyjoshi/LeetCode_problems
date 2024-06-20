a9973e8011e2281777851019e58456ab42c1d383
Amey Joshi <ameyjoshi2001rj@gmail.com>
2024-06-20 17:49:37 +0530

    Time: 27 ms (6.06%), Space: 43 MB (71.90%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+          return false;
+        }
+      
+      HashMap <Character,Integer> mp = new HashMap<>();
+      for(int i = 0;i<s.length();i++){
+        mp.put(s.charAt(i),mp.getOrDefault(s.charAt(i),0) + 1);
+        mp.put(t.charAt(i),mp.getOrDefault(t.charAt(i),0) - 1);
+      }
+      
+      for(char c : mp.keySet()){
+        if(mp.get(c) != 0) return false;
+      }
+      
+      return true;
+    }
+}

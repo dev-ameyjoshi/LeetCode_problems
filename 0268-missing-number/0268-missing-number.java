df85aad1942f729909676081f89647bdb3d31fd2
Amey Joshi <ameyjoshi2001rj@gmail.com>
2024-10-12 16:33:45 +0530

    Time: 0 ms (100.00%), Space: 45 MB (83.98%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,17 +1,16 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n = nums.length;
+        int xor_one=0;
+        int xor_two=0;
+        int N = nums.length;
+       
       
-        Set<Integer> numSet = new HashSet<Integer>();
-        for(int num:nums) numSet.add(num);
-        
-        int expectedNumCount = nums.length+1;
-        
-        for(int i = 0;i<expectedNumCount;i++){
-          if(!numSet.contains(i)){
-            return i;
-          }
+        for(int i = 0;i<N;i++){
+          xor_two = xor_two ^ nums[i];
+          xor_one = xor_one ^ (i+1); 
         }
-      return -1;
+   
+      
+      return xor_one ^ xor_two;
     }
 }

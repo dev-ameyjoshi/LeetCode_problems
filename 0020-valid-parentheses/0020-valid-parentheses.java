7279365927b5488819b3c9331c21f12021d974a9
Amey Joshi <ameyjoshi2001rj@gmail.com>
2024-10-13 12:54:22 +0530

    Time: 4 ms (7.66%), Space: 42.1 MB (6.92%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,32 +1,34 @@
 class Solution {
-    
-    private HashMap<Character,Character> mappings;
+    //To create a key value pair to map value we have to the key the mappings.
+    private HashMap <Character,Character> mappings;
     
     public Solution(){
-        this.mappings  = new HashMap<Character,Character>();
-        this.mappings.put(')','(');
-        this.mappings.put('}','{');
-        this.mappings.put(']','[');
+      this.mappings = new HashMap<Character,Character>();
+      this.mappings.put('}','{');
+      this.mappings.put(')','(');
+      this.mappings.put(']','[');
     }
     public boolean isValid(String s) {
-      Stack<Character> stack = new Stack<Character>();  
-      for(int i = 0;i<s.length();i++){
+        Stack<Character> stack = new Stack<Character>();
+      
+        for(int i = 0;i<s.length();i++){
           char c = s.charAt(i);
-          
-          if(this.mappings.containsKey(c)){
-              char topElement = stack.empty() ? '#': stack.pop(); 
-              if(topElement != this.mappings.get(c)){
-              return false;
-          }
-          }else{
-              stack.push(c);
-          }
-          
-          
-      }
+
+            if(this.mappings.containsKey(c)){
+
+                char topElement = stack.empty() ? '#' : stack.pop();
+
+                if(topElement != this.mappings.get(c)){
+                  return false;
+                }
+            } else{
+                  stack.push(c);
+                }
+
+            }
+         
         
-        return stack.isEmpty();
-        
-        
+      
+      return stack.isEmpty();
     }
 }

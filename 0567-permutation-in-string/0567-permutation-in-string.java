03f0bdf0391a61e7ce5196e009e39cfab9747a3c
Amey Joshi <ameyjoshi2001rj@gmail.com>
2024-11-16 09:26:05 +0530

    Time: 7 ms (72.76%), Space: 42.2 MB (90.42%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,31 +1,32 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-     
-        if (s1.length()>  s2.length() ||  s2.length() ==0){
-            return false;
+        int n1 = s1.length();
+        int n2 = s2.length();
+        int l = 0;
+        //base cases : 
+        if(s1.length() > s2.length() || s2.length() == 0){
+          return false;
         }
-        int n1 = s1.length(), n2 = s2.length();
-        if(n1 ==0) return true;
-         
-        int[] arr1 = new int[26];
-        int[] arr2 = new int[26];
-        for (int i = 0;i<n1;i++){
-            arr1[s1.charAt(i) - 'a']++;
-            arr2[s2.charAt(i) - 'a']++;
+      
+        if(n1 == 0) return true;
+        int [] arr1 = new int[26];
+        int [] arr2 = new int[26];
+      
+        for(int i = 0;i < n1;i++){
+          arr1[s1.charAt(i) - 'a']++;
+          arr2[s2.charAt(i) - 'a']++;
         }
-        
-        for (int i = n1; i < n2; i++)
-        {
-            if(Arrays.equals(arr1,arr2)){
-              return true;  
-            } 
-            arr2[s2.charAt(i-n1) - 'a']--;
-            arr2[s2.charAt(i) - 'a']++;
-            
+      
+        for(int i = n1;i < n2;i++){
+           if(Arrays.equals(arr1,arr2)){
+             return true;
+           }
+          
+          arr2[s2.charAt(i-n1) - 'a']--;
+          arr2[s2.charAt(i) - 'a']++;
+          
         }
-        if (Arrays.equals(arr1, arr2)) return true;
-                
-        else return false;
-       
+      if(Arrays.equals(arr1,arr2)) return true;
+      else return false;
     }
 }

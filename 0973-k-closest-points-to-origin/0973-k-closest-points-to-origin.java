2f4247d67595e4528ea0bd85e04fd9e62c3d9e73
Amey Joshi <ameyjoshi2001rj@gmail.com>
2024-12-14 18:49:27 +0530

    Time: 33 ms (40.20%), Space: 51.4 MB (65.38%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,41 +1,20 @@
 class Solution {
-    class Pair{
-      int distance,x,y;
-      Pair(int distance,int x,int y){
-        this.distance = distance;
-        this.x = x;
-        this.y = y;
-      }
-      
-    }
-    
     public int[][] kClosest(int[][] points, int k) {
-    // using the formula for distance between 2 points _/x^2 + y^2
-      PriorityQueue<Pair> maxh = new PriorityQueue<>((a,b) -> b.distance - a.distance);
+PriorityQueue<int[]> maxHp = new PriorityQueue<>((a,b) -> Integer.compare(b[0] *b[0] + b[1] *b[1], a[0] * a[0] + a[1] * a[1]));
       
-   
-      int arr [][] = new int [k][points[0].length]; 
-     
-      int idx = 0;
-      for( int []temp : points){
-        
-        int distance = temp[0]*temp[0] + temp[1]*temp[1]; 
-        maxh.add(new Pair(distance,temp[0],temp[1]));
-        
-        if(maxh.size()> k) maxh.poll();    
-            
+      for(int [] point : points){
+        maxHp.offer(point);
+        if(maxHp.size() >  k){
+          maxHp.poll();
+        }
       }
       
+      int [][] res = new int[k][2];
+      int i = 0;
       
-      while(!maxh.isEmpty()){
-        int xCoordinate = maxh.peek().x;
-        int yCoordinate = maxh.peek().y;
-        arr[idx][0] = xCoordinate;
-        arr[idx][1] = yCoordinate;
-        idx++;
-        maxh.poll();
+      while(!maxHp.isEmpty()){
+        res[i++] = maxHp.poll();
       }
-      return arr;
-        
+      return res;
     }
 }
